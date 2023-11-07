2e26abc8a8bdbb04e3111522f16ba5a6a7b64823
Ken Raeburn <raeburn@redhat.com>
2023-11-06 22:30:52 -0500

    dm vdo: function declaration and call reflowing: permassert through repair
    Mechanical rewrapping (with minor manual patching) of function
    declarations, definition headers, and function calls to better
    approximate device-mapper style, using 90-column soft line-length
    limit.
    
    Signed-off-by: Ken Raeburn <raeburn@redhat.com>
    Signed-off-by: Matthew Sakai <msakai@redhat.com>

--- drivers/md/dm-vdo/permassert.c
+++ drivers/md/dm-vdo/permassert.c
@@ -8,25 +8,16 @@
 #include "errors.h"
 #include "logger.h"
 
-int uds_assertion_failed(const char *expression_string,
-			 const char *file_name,
-			 int line_number,
-			 const char *format,
-			 ...)
+int uds_assertion_failed(const char *expression_string, const char *file_name,
+			 int line_number, const char *format, ...)
 {
 	va_list args;
 
 	va_start(args, format);
 
-	uds_log_embedded_message(UDS_LOG_ERR,
-				 UDS_LOGGING_MODULE_NAME,
-				 "assertion \"",
-				 format,
-				 args,
-				 "\" (%s) failed at %s:%d",
-				 expression_string,
-				 file_name,
-				 line_number);
+	uds_log_embedded_message(UDS_LOG_ERR, UDS_LOGGING_MODULE_NAME, "assertion \"",
+				 format, args, "\" (%s) failed at %s:%d",
+				 expression_string, file_name, line_number);
 	uds_log_backtrace(UDS_LOG_ERR);
 
 	va_end(args);
